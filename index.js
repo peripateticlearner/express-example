fa680a7ffc5b07f1bcc1c5c02d747f2914d52765
Ali Kirat <91100475+peripateticlearner@users.noreply.github.com>
2025-03-12 08:02:27 -0700

    updated

--- index.js
+++ index.js
@@ -1,75 +1,85 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const PORT = 3000;
+const express = require('express')
+const bodyParser = require('body-parser')
 
-// Importing the data from our fake database files.
-const users = require('./data/users');
-const posts = require('./data/posts');
+const app = express()
+const PORT = 3000
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json({extended: true}))
+
+const users = require("./data/users")
+const posts = require("./data/posts")
+
+// console.log(posts);
+// console.log(users);
 
 app.get('/', (req, res) => {
-    res.send("Base Home Page");
-});
+    res.send("Base home page")
+})
 
-// GET all users - Fetches the entire list of users
-app.get('/users', (req, res) => {
-    res.json(users);
-});
+app
+    .route('/users')
+    .get((req, res) => {
+    res.json(users)
+})
+    .post((req, res) => {
+        if(req.body.name && req.body.username && req.body.email){
+            if(users.find((user)=> user.username == req.body.username)){
+                res.json({error: "Username already exists"})
+                return
+            }
+        }
+        const user = {
+            id: users[users.length -1].id + 1,
+            name: req.body.name,
+            username: req.body.username,
+            email: req.body.email,
+        }
+        users.push(user)
+        res.json(users[users.length -1])
+    })
 
-// POST /users - Adds a new user to the database
-app.post('/users', (req, res) => {
-    if (!req.body.name || !req.body.username || !req.body.email) {
-        return res.status(400).json({ error: "Insufficient Data" });
-    }
-
-    if (users.find(user => user.username === req.body.username)) {
-        return res.status(400).json({ error: "Username already exists" });
-    }
-
-    const newUserId = users.length > 0 ? users[users.length - 1].id + 1 : 1;
-
-    const user = {
-        id: newUserId,
-        name: req.body.name,
-        username: req.body.username,
-        email: req.body.email
-    };
-
-    users.push(user);
-    res.status(201).json(user);
-});
-
-// GET all posts - Returns all posts from the database
 app.get('/posts', (req, res) => {
-    res.json(posts);
-});
+    res.json(posts)
+})
 
-// GET individual user by ID
-app.get('/users/:usersID', (req, res) => {
-    const user = users.find(user => user.id === Number(req.params.usersID));
-    
-    if (!user) {
-        return res.status(404).json({ error: "User not found" });
-    }
-    
-    res.json(user);
-});
+app
+    .route('/users/:usersID')
+    .get((req, res) => {
+    //res.send(`User ID: ${req.params.usersID}`)
+    const user = users.find((user)=> user.id == req.params.usersID)
+    if(user) {res.json(user)}
+    else {res.status(404).send("User not found")}
+})
+    .patch((req, res) => {
+        const user = users.find((user, i) => {
+            if(user.id == req.params.usersID){
+                for (const key in req.body){
+                users[i][key] = req.body[key]
+            }
+            return true
+        }
+    })
+    if (user) res.json(user)
+    else {res.status(404).send("User not found")}
+    })
+    .delete((req, res) => {
+        const user = users.find((user, i) => {
+            if(user.id == req.params.usersID){
+                users.splice(i, 1)
+                return true
+            }
+        })
+        if (user) res.json(user)
+        else {res.status(404).send("User not found")} 
+    })
 
-// GET individual post by ID
 app.get('/posts/:postsID', (req, res) => {
-    const post = posts.find(post => post.id === Number(req.params.postsID));
+    const post = posts.find((post) => post.id == req.params.postsID)
+    if (post) {res.json(post)}
+    else{res.status(404).send("Post not found")}
+})
 
-    if (!post) {
-        return res.status(404).json({ error: "Post not found" });
-    }
-
-    res.json(post);
-});
-
-// Server start - Logs the port when the server is running
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+    console.log(`Server is running on port ${PORT}`);
+})
